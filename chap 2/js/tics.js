2c8ad9ac995009b1b96430eb3f6d4b075d619acb
jeffrey osayande <sajeff2003@yahoo.com>
2019-08-12 21:20:43 -0400

    chapter 2 q3

---
+++ chap 2/js/tics.js
@@ -0,0 +1,14 @@
+console.log("Hello tickets")
+
+let age=prompt("enter your age")
+
+
+    if(age<5){
+	console.log("free")
+	}
+	else if(age>=5 && age<=12){
+	console.log("$5.00")
+	}
+	else if(age>12){
+	console.log("$9.00")
+	}
