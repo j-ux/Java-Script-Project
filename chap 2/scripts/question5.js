2c7e3fa58465b37a698647d9e225ce4d5338cb9d
jeffrey osayande <sajeff2003@yahoo.com>
2019-08-13 20:01:13 -0400

    chp2 quest 3 and ques 5

---
+++ chap 2/scripts/question5.js
@@ -0,0 +1,12 @@
+console.log("Hello expressions")
+
+let number=prompt("enter a number")
+
+
+    if(number>=1 && number<=50){
+	console.log("Within Range")
+	}
+	
+	else if(number<1 || number>50){
+	console.log("Outside Range")
+	}
