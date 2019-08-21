3145bd63ddaa5127efab8cefa2c164e69c7ec9cd
jeffrey osayande <sajeff2003@yahoo.com>
2019-08-20 21:11:40 -0400

    chap5 question7 added

---
+++ Chap 5/scripts/chap5Q7.js
@@ -0,0 +1,47 @@
+
+let itr=prompt("how many numbers do you want to enter")
+
+let evenarray=[ ]
+
+let oddarray=[ ]
+
+let even_sum=0
+
+let odd_sum=0
+
+let evenavg=0
+
+let oddavg=0
+
+   for(let i=1; i<=itr; i++){
+	   
+	   let num=prompt("enter a number")
+   
+        if(num%2==0){
+			evenarray.push(num);
+			even_sum=even_sum + i;
+			
+		}
+		else{
+			oddarray.push(num);
+			odd_sum=odd_sum + i;
+			
+		}
+		
+		}
+		
+		evenavg=(even_sum/evenarray.length);
+		
+		oddavg=(odd_sum/oddarray.length);
+		
+		console.log("sum evenarray is" + even_sum);
+		
+		console.log("avg evenarray is" + evenavg);
+		
+		console.log("sum oddarray is" + odd_sum);
+		
+		console.log("avg oddarray is" + oddavg);
+		
+		
+		
+	
