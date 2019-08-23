c73bfc5c2a0ef7bbc9fe1a9729d2a024e7447573
JayKumar223 <53454284+JayKumar223@users.noreply.github.com>
2019-08-22 21:08:20 -0400

    question 6 chapter 2

---
+++ chap 2/Chapter2q6.js
@@ -0,0 +1,40 @@
+
+function answer1(){
+	let user1=document.getElementById('question1').value
+	
+	if (user1==14)
+			alert("correct")
+	
+	else 
+		alert("incorrect")
+}
+
+function answer2(){
+	let user1=document.getElementById('question2').value
+	
+	if (user1==24)
+			alert("correct")
+	
+	else 
+		alert("incorrect")
+}
+
+function answer3(){
+	let user1=document.getElementById('question3').value
+	
+	if (user1==9)
+			alert("correct")
+	
+	else 
+		alert("incorrect")
+}
+
+function answer4(){
+	let user1=document.getElementById('question4').value
+	
+	if (user1==16)
+			alert("correct")
+	
+	else 
+		alert("incorrect")
+}
